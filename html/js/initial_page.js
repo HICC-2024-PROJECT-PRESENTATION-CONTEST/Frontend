2b0dc01852aa6f988b01c78ea0c6b531927a1ffd
Leejiwoo011201 <jiwoomiso@gmail.com>
2024-08-18 14:29:27 +0900

    feat/initial-page

---
+++ html/js/initial_page.js
@@ -0,0 +1,11 @@
+script.js;
+function getCurrentDate() {
+  const today = new Date();
+  const year = today.getFullYear().toString().slice(2);
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
+}
+
+document.getElementById('noticeDate').textContent = getCurrentDate();
