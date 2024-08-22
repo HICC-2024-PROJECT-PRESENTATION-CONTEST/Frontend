bff4e0a4cd51d0204ef7aaf5f92691f1ffd8f09c
Leejiwoo011201 <jiwoomiso@gmail.com>
2024-08-23 01:36:31 +0900

    수정

---
+++ html/js/header_button.js
@@ -0,0 +1,6 @@
+document.getElementById('login-button').addEventListener('click', function () {
+  window.location.href = 'login_page.html';
+});
+document.getElementById('home-button').addEventListener('click', function () {
+  window.location.href = 'Initial_page.html';
+});
